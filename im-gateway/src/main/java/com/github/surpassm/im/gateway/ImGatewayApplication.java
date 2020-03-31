9d21c8bd3b0b4a27a25284f4c5a586ca7145bb02
lustres@sina.com <lustres@sina.com>
2020-03-31 12:50:29 +0800

    修正

--- im-gateway/src/main/java/com/github/surpassm/im/gateway/ImGatewayApplication.java
+++ im-gateway/src/main/java/com/github/surpassm/im/gateway/ImGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.github.surpassm.iot.gateway;
+package com.github.surpassm.im.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-public class IotGatewayApplication {
+public class ImGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IotGatewayApplication.class, args);
+        SpringApplication.run(ImGatewayApplication.class, args);
     }
 
 }
